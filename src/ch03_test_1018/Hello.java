343554434a6b077b32270d4c09625f8e879e66cd
eshehh <qotjsgks1092@naver.com>
2022-11-24 11:56:38 +0900

    수업 예제 저장 1124 3교시

--- src/ch03_test_1018/Hello.java
+++ src/ch03_test_1018/Hello.java
@@ -15,6 +15,7 @@ public class Hello {
 		System.out.println("Hello 이상용 자바");
 		System.out.println("Hello 이상용 자바");
 		System.out.println("Hello 이상용 자바");
+		System.out.println("Hello 이상용 자바");
 	}
 
 }
